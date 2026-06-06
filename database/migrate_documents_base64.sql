92e57d2ef6bd964c4ac2e526643e11b8c8136209
Rémi <58167960+D-Remi@users.noreply.github.com>
2026-06-06 10:56:59 +0200

    commit version 40.1

--- database/migrate_documents_base64.sql
+++ database/migrate_documents_base64.sql
@@ -1,7 +1,5 @@
--- Migration documents : ajouter stockage base64
+-- Migration documents : stockage base64
 ALTER TABLE `documents`
   ADD COLUMN IF NOT EXISTS `data`     longtext DEFAULT NULL AFTER `filename`,
   ADD COLUMN IF NOT EXISTS `mimetype` varchar(100) DEFAULT NULL AFTER `data`;
-
--- L'ancienne colonne url n'est plus utilisée mais on la garde pour compatibilité
 SELECT 'Migration documents base64 OK' AS statut;
